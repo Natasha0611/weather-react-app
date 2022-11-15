5a8e51d71e5fe60a46a1febbb22717fd046fa381
Natasha0611 <lunataly06@gmail.com>
2022-11-15 21:01:44 +0000

    Added GIT url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ function App() {
   return (
     <div className="App">
      Weather App
+     <footer>
+      This project was codded by <a href="https://my-profile-nd.netlify.app" target="_blank">Nataliia Dolhoruk</a> and it is {" "}
+      <a href="https://github.com/Natasha0611/weather-react-app" target="_blank"> open-sourced on GIT</a>
+     </footer>
     </div>
   );
 }
