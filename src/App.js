da60ed6acf0420558721021dd7b1724edf4423df
Natasha0611 <lunataly06@gmail.com>
2022-11-15 22:02:58 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+  <div className="App">
+    <div className='container'>
      Weather App
      <footer>
-      This project was codded by <a href="https://my-profile-nd.netlify.app" target="_blank">Nataliia Dolhoruk</a> and it is {" "}
-      <a href="https://github.com/Natasha0611/weather-react-app" target="_blank"> open-sourced on GIT</a>
+      This project was codded by <a href="https://my-profile-nd.netlify.app">Nataliia Dolhoruk</a> and it is {" "}
+      <a href="https://github.com/Natasha0611/weather-react-app"> open-sourced on GIT</a>
      </footer>
     </div>
+  </div>
   );
 }
 
