e7c8ad3771f37a2903b70cb03a784fd89ceb1426
Natasha0611 <lunataly06@gmail.com>
2022-11-16 18:18:18 +0000

    Add component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
   <div className="App">
     <div className='container'>
      Weather App
+     <Weather />
      <footer>
       This project was codded by <a href="https://my-profile-nd.netlify.app">Nataliia Dolhoruk</a> and it is {" "}
       <a href="https://github.com/Natasha0611/weather-react-app"> open-sourced on GIT</a>
