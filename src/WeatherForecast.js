bee4a99c9cf8f57dc0980238aaed960e611c917a
Natasha0611 <lunataly06@gmail.com>
2022-11-23 22:33:34 +0000

    Add daily forecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props){
+const[loaded, setLoaded] = useState(false);
+const[forecast, setForecast] = useState(null);
+   function handleResponse(response){
+        console.log(response);
+        setForecast(response.data.daily);
+        setLoaded(true);
+    }
+    if(loaded){
+        return(
+            <div className="WeatherForecast">
+                <div className="row">
+                    {forecast.map(function(dailyForecast, index){
+                    if (index < 6) {
+                            return (
+                            <div className="col" key={index}>
+                             <WeatherForecastDay data={dailyForecast} />
+                             </div>
+                            );
+                          }
+                    })}
+                </div>
+            </div>
+        );
+    }
+    else{
+        const apiKey = `907fef83425ee6575ce345a2f87d8989`;
+        let longitude = props.coordinates.lon;
+        let latitude = props.coordinates.lat;
+        let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+        axios.get(apiUrl).then(handleResponse);
+        return null;
+  
+    }
+}
